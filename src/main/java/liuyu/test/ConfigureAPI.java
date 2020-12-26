886bf5be35f2454c85993018033ab862ded99bb6
liuyu <liuyu@192.168.1.8>
2020-12-26 20:48:12 +0800

    修改123

--- src/main/java/liuyu/test/ConfigureAPI.java
+++ src/main/java/liuyu/test/ConfigureAPI.java
@@ -3,7 +3,6 @@ package liuyu.test;
 public class ConfigureAPI {
 
 	public interface KafkaProperties {
-		
         public final static String ZK = "192.168.0.99:2181,192.168.0.98:2181,192.168.0.96:2181";
         public final static String GROUP_ID = "test_group1";
         public final static String TOPIC = "liuyu1";
